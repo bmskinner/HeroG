778807982c4a683ab2a02c09319c328fd284cdf8
Ben Skinner <bms41@cam.ac.uk>
2018-11-27 15:15:33 +0000

    Added subtraction of sex and season data from birthweight data.

--- src/findSVAProbesOverlappingGenes.R
+++ src/findSVAProbesOverlappingGenes.R
@@ -2,7 +2,7 @@
 # Export the CpGs and the unique genes to separate files
 
 # Install and load packages
-setUpWorkSpace = function() {
+setUpWorkSpace = function(packages, biopackages) {
   
   source("https://bioconductor.org/biocLite.R")
   
@@ -15,22 +15,19 @@ setUpWorkSpace = function() {
   
   installIfNeeded = function(pkg){
     if(pkg %in% rownames(installed.packages()) == FALSE) {
-      install.packages(pkg)
+      install.packages(pkg, dependencies = TRUE)
     }
     suppressPackageStartupMessages(library(pkg, character.only = TRUE, warn.conflicts = FALSE))
   }
-  
-  
-  biopackages = c("BiSeq", "GenomicRanges", "GenomeGraphs", "biomaRt", "BiSeq", "rtracklayer" )
+
   lapply(biopackages, installIfNeededFromBioconductor)
-  
-  packages = c("dplyr", "ggplot2", "ggbio", "RCircos")
   lapply(packages, installIfNeeded)
   
   source("readMethylationSamples.R") # simplify reading of bismark files
 }
 
-setUpWorkSpace()
+setUpWorkSpace(packages    = c("dplyr", "ggplot2", "RCircos"),   
+               biopackages = c("GenomicRanges", "GenomeGraphs", "ggbio", "rtracklayer" ))
 
 # Generate a human reference genome
 createReferenceGenome = function(){
@@ -131,20 +128,28 @@ find.overlapping.genes = function(ranges.to.annotate, file.name){
 b.file = paste0(working.dir, "SVA.birth.10_reads.0.99_cov.0.01_diff.csv")
 b.data = make.granges.from.sva.file(b.file)
 
-
 # Read sex associated CpGs
 s.file = paste0(working.dir, "SVA.sex.10_reads.0.99_cov.0.01_diff.csv")
 s.data = make.granges.from.sva.file(s.file)
 
-# Find overlaps
+n.file = paste0(working.dir, "SVA.season.10_reads.0.99_cov.0.01_diff.csv")
+n.data = make.granges.from.sva.file(n.file)
+
+# Find overlaps between birthweight and sex
 overlaps = findOverlaps(b.data, s.data)
 o.data   = b.data[queryHits(overlaps)]
 
-# Find genes associated with each
+# Subtract sex and season CpGs from birthweight to remove loci 
+# resulting from an interaction or either
+bw.only.hits = setdiff(b.data, s.data)
+bw.only.hits = setdiff(bw.only.hits, n.data)
 
+
+# Find genes associated with each CpG
 find.overlapping.genes(b.data, "CpGs.in.genes.associated.with.birthweight.tsv")
 find.overlapping.genes(s.data, "CpGs.in.genes.associated.with.sex.tsv")
+find.overlapping.genes(n.data, "CpGs.in.genes.associated.with.season.tsv")
 find.overlapping.genes(o.data, "CpGs.in.genes.associated.with.birthweight.and.sex.tsv")
-
+find.overlapping.genes(bw.only.hits, "CpGs.in.genes.associated.with.birthweight.subtracted_sex_season.tsv")
 
 
